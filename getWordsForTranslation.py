d701a6e98dfb03a59f1b223d2870b724c5636ae7
Pomogadryg <denver1033@gmail.com>
2023-06-30 12:49:20 +0300

    added main2 with description

--- getWordsForTranslation.py
+++ getWordsForTranslation.py
@@ -10,17 +10,20 @@ class ReadDocx():
         self.table = doc.tables[0]
         # self.read_words()
     def read_words(self):
-        final_dic = {}
+        final_dic_eng_rus = {}
+        final_dic_eng_mean = {}
         # Get the second column of the table
         column2 = self.table.columns[1]
         column3 = self.table.columns[2]
+        column4 = self.table.columns[3]
         list_ = ["–","-"]
         # Loop through each cell in the column
         count=0
-        for cell_eng,cell_rus in zip(column2.cells,column3.cells):
+        for cell_eng,cell_rus, cell_mean in zip(column2.cells,column3.cells,column4.cells):
             # Get the text from the cell
             text_eng = cell_eng.text.strip()
             text_rus = cell_rus.text.rstrip("-")
+            meaning_eng = cell_mean.text.strip()
             # Find the index of the "-/" characters in the text
             for i in list_:
                 index = text_eng.find(i)
@@ -28,8 +31,9 @@ class ReadDocx():
                     break
             # If the characters are found, print out the text before them
             if index != -1:
-                final_dic[text_eng[:index].strip()]=text_rus
+                final_dic_eng_rus[text_eng[:index].strip()]=text_rus
+                final_dic_eng_mean[text_eng[:index].strip()] = meaning_eng
                 count+=1
-        return(final_dic)
+        return(final_dic_eng_rus,final_dic_eng_mean)
 
 # s=ReadDocx()
--- main2.py
+++ main2.py
@@ -3,6 +3,7 @@ from getWordsForTranslation import ReadDocx
 import random
 from to_save_words import WordsToSave
 from PIL import Image, ImageTk
+import textwrap
 
 
 get_words = ReadDocx()
@@ -11,6 +12,11 @@ words_dic, words_meaning = get_words.read_words()
 eng_w = [k for k,v in words_dic.items()]
 en_in_eng_v = str()
 ru_in_eng_v = str()
+mean_in_eng_v = str()
+
+
+eng_mean = [k for k,v in words_meaning.items()]
+
 
 
 
@@ -28,6 +34,11 @@ question_font = ("Helvetica",30)
 answer_font = ("Helvetica", 23)
 score_font= ("Helvetica",15)
 
+def wrap_text(text, width):
+    wrapper = textwrap.TextWrapper(width=width)
+    wrapped_text = wrapper.fill(text)
+    return wrapped_text
+
 def check_result(what_was_clicked):
     rus_list_to_check_whatWasClicked= "абвгдежзийклмнопрстуфхцчшщъыьэюяАБВГДЕЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ"
     check_en_or_rus = str()
@@ -35,6 +46,7 @@ def check_result(what_was_clicked):
     global ru_in_eng_v
     global en_in_rus_v
     global ru_in_rus_v
+    global mean_in_eng_v
 
     global total_clicked
     global match_clicked
@@ -43,9 +55,13 @@ def check_result(what_was_clicked):
 
     if what_was_clicked[0] in rus_list_to_check_whatWasClicked or what_was_clicked[-1] in rus_list_to_check_whatWasClicked:
         check_en_or_rus = ru_in_eng_v
+    
+    elif en_in_rus_v:
+        check_en_or_rus = en_in_rus_v
     else:
-        check_en_or_rus = en_in_rus_v        
-    if (what_was_clicked in check_en_or_rus ):
+        check_en_or_rus = mean_in_eng_v
+
+    if (what_was_clicked in check_en_or_rus):
         match_clicked+=1
         score_label.config(text=f"Correct!\nscore {match_clicked}/{total_clicked+1}, {round(match_clicked/(total_clicked+1) * 100, 2)}%\n")
         startGame()
@@ -65,6 +81,7 @@ def startGame():
     def eng_turn():
         global en_in_eng_v
         global ru_in_eng_v
+        global mean_in_eng_v
         en_in_eng_v = random.choice(eng_w)
         en_idx = eng_w.index(en_in_eng_v)
         ru_in_eng_v = words_dic[en_in_eng_v]
@@ -86,6 +103,35 @@ def startGame():
             extra_button[idx].config(text=a,font=answer_font)
             extra_button[idx].config(command=lambda content=a: check_result(content))
             extra_button[idx].grid(row=3+idx,column=1, sticky="nsew")
+
+    def eng_meaning():
+        global en_in_eng_v
+        # global ru_in_eng_v
+        global mean_in_eng_v
+        en_in_eng_v = random.choice(eng_w)
+        en_idx = eng_w.index(en_in_eng_v)
+        mean_in_eng_v = words_meaning[en_in_eng_v]
+        if mean_in_eng_v == "":
+            eng_turn()
+        else:
+            main_label.config(text=f"{en_in_eng_v}")
+            original_text_scoreLabel=score_label.cget("text")
+            score_label.config(text=f"{original_text_scoreLabel}\n{en_idx}/{len(eng_w)}")
+            list_extra_answers = []
+            for i in range(5):
+                if i == 0:
+                    list_extra_answers.append(mean_in_eng_v)
+                else:
+                    w = random.choice(eng_w)
+                    if words_meaning[w] not in list_extra_answers:
+                        list_extra_answers.append(words_meaning[w])
+                    else:
+                        i-=1
+            new_list_extra_ansers = random.sample(list_extra_answers,len(list_extra_answers))
+            for idx,a in enumerate(new_list_extra_ansers):
+                extra_button[idx].config(text= wrap_text(a,width=40),font=answer_font)
+                extra_button[idx].config(command=lambda content=a: check_result(content))
+                extra_button[idx].grid(row=3+idx,column=1, sticky="nsew")
 
     
     def rus_turn():
@@ -114,15 +160,18 @@ def startGame():
             extra_button[idx].config(command=lambda content=a: check_result(content))
             extra_button[idx].grid(row=3+idx,column=1, sticky="nsew")
 
-    what_to_start = random.choice([eng_turn, rus_turn])
+    # what_to_start = random.choice([eng_turn, rus_turn])
+    what_to_start = random.choice([eng_meaning])
 
     what_to_start()
 
 score_label = Label(text="score", font=score_font, bg="lightblue")
 score_label.grid(row=1,column=1, sticky="nsew")
 
+
 main_label = Label(text="", font=question_font, padx=50, pady=20, bg="lightgreen")
 main_label.grid(row=2,column=1, sticky="nsew")
+
 
 
 
